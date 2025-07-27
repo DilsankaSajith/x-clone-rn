59f483b526198350fd91140bb14cd423780421b5
Dilsanka Sajith <Dilsanka39@gmail.com>
2025-07-28 00:58:13 +0530

    server fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
-import { ENV } from './config/env.js';
-import { connectDB } from './config/db.js';
 
-// Routes
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import notificationRoutes from './routes/notification.route.js';
+
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 import { arcjetMiddleware } from './middleware/arcjet.middleware.js';
 
 const app = express();
 
-app.use(cors);
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
+
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => res.send('Hello from server'));
+
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
@@ -33,13 +35,15 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
+
+    // listen for local development
     if (ENV.NODE_ENV !== 'production') {
       app.listen(ENV.PORT, () =>
         console.log('Server is up and running on PORT:', ENV.PORT)
       );
     }
   } catch (error) {
-    console.error('Failed to start server: ', error.message);
+    console.error('Failed to start server:', error.message);
     process.exit(1);
   }
 };
